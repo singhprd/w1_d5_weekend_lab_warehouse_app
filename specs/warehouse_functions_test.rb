f0f57510dfc1527678fd19263b42d05800ab635c
Peter Singh <singh.prd@gmail.com>
2016-02-20 15:24:31 +0000

    added function for return_items_from_bays. Also added 2 tests for this function.

--- specs/warehouse_functions_test.rb
+++ specs/warehouse_functions_test.rb
@@ -6,17 +6,36 @@ class TestWarehouse < MiniTest::Test
   def setup
 
   @warehouse_product_information_hash = {
-
-  a1:  'needle'       ,
-  a2:  'stop sign'    ,
-  a3:  'blouse'       ,
-  a4:  'hanger'       ,
-  a5:  'rubber duck'  ,
-  a6:  'shovel'       ,
-  a7:  'bookmark'     ,
-  a8:  'model car'    ,
-  a9:  'glow stick'   ,
-  a10: 'rubber band'
+  a1:  'needle'         ,
+  a2:  'stop sign'      ,
+  a3:  'blouse'         ,
+  a4:  'hanger'         ,
+  a5:  'rubber duck'    ,
+  a6:  'shovel'         ,
+  a7:  'bookmark'       ,
+  a8:  'model car'      ,
+  a9:  'glow stick'     ,
+  a10: 'rubber band'    ,
+  b1:  'tyre swing'     ,
+  b2:  'sharpie'        ,
+  b3:  'picture frame'  ,
+  b4:  'photo album'    ,
+  b5:  'nail filer'     ,
+  b6:  'tooth paste'    ,
+  b7:  'bath fizzers'   ,
+  b8:  'tissue box'     ,
+  b9:  'deodorant'      ,
+  b10: 'cookie jar'     ,
+  c1:  'rusty nail'     ,
+  c2:  'drill press'    ,
+  c3:  'chalk'          ,
+  c4:  'word search'    ,
+  c5:  'thermometer'    ,
+  c6:  'face wash'      ,
+  c7:  'paint brush'    ,
+  c8:  'candy wrapper'  ,
+  c9:  'shoe lace'      ,
+  c10: 'leg warmers'    
 }
   end
 
@@ -36,8 +55,14 @@ class TestWarehouse < MiniTest::Test
   # # - given "b5, b10, and b6", determine that the products are "nail filer, cookie jar, and tooth paste", and they're five bays apart
 
   def test_return_list_items_from_bays
-    actual_return = return_list_items_from_bays( [ 'nail filer', 'cookie jar', 'tooth paste'], @warehouse_product_information_hash )
+    actual_return = return_list_items_from_bays( [ 'b5', 'b10', 'b6' ], @warehouse_product_information_hash )
     expected_return = [ 'nail filer', 'cookie jar', 'tooth paste' ]
+    assert_equal( expected_return, actual_return )
+  end
+
+  def test_return_list_items_from_bays_not_found
+    actual_return = return_list_items_from_bays( [ 'b500', 'b100', 'b600' ], @warehouse_product_information_hash )
+    expected_return = [ "Bay b500 not found", "Bay b100 not found", "Bay b600 not found" ]
     assert_equal( expected_return, actual_return )
   end
 
--- warehouse_picker_app.rb
+++ warehouse_picker_app.rb
@@ -2,18 +2,36 @@ require_relative( 'warehouse_functions' )
 
 # Data
 product_info_hash  = {
-
-  a1:  'needle'       ,
-  a2:  'stop sign'    ,
-  a3:  'blouse'       ,
-  a4:  'hanger'       ,
-  a5:  'rubber duck'  ,
-  a6:  'shovel'       ,
-  a7:  'bookmark'     ,
-  a8:  'model car'    ,
-  a9:  'glow stick'   ,
-  a10: 'rubber band'  ,
-
+  a1:  'needle'         ,
+  a2:  'stop sign'      ,
+  a3:  'blouse'         ,
+  a4:  'hanger'         ,
+  a5:  'rubber duck'    ,
+  a6:  'shovel'         ,
+  a7:  'bookmark'       ,
+  a8:  'model car'      ,
+  a9:  'glow stick'     ,
+  a10: 'rubber band'    ,
+  b1:  'tyre swing'     ,
+  b2:  'sharpie'        ,
+  b3:  'picture frame'  ,
+  b4:  'photo album'    ,
+  b5:  'nail filer'     ,
+  b6:  'tooth paste'    ,
+  b7:  'bath fizzers'   ,
+  b8:  'tissue box'     ,
+  b9:  'deodorant'      ,
+  b10: 'cookie jar'     ,
+  c1:  'rusty nail'     ,
+  c2:  'drill press'    ,
+  c3:  'chalk'          ,
+  c4:  'word search'    ,
+  c5:  'thermometer'    ,
+  c6:  'face wash'      ,
+  c7:  'paint brush'    ,
+  c8:  'candy wrapper'  ,
+  c9:  'shoe lace'      ,
+  c10: 'leg warmers'    
 }
 
 # 1)
@@ -37,12 +55,23 @@ puts "#{user_input_2}: #{return_bay_from_item( user_input_2, product_info_hash )
   # 3) Given a list of bays, list the items in those bays
   # - given "b5, b10, and b6", determine that the products are "nail filer, cookie jar, and tooth paste", and they're five bays apart
 
+puts "Input list of bays to return a list of items. Input bay or 'get' to return information:"
 
+# declares variables needed 
+user_input_array = []
+user_input_raw = 'nil'
 
+# runs a while loop to input any number of bays. 
+# the loop exits when the user inputs 'get'. 'get' is added to the array but .pop removes it in the next line.
+  while user_input_raw != 'get'
+    print ':' 
+    user_input_raw = gets.chomp
+    user_input_array << user_input_raw 
+  end
 
+user_input_array.pop  
 
-
-
+puts return_list_items_from_bays( user_input_array, product_info_hash )
 
 
 
